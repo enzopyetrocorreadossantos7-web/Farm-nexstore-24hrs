df9871c256d75bab1ea29dbf092ec660bb7aca97
enzopyetrocorreadossantos7-web <enzopyetrocorreadossantos7@gmail.com>
2026-03-19 18:55:25 -0400

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,8 +1,11 @@
 const http = require("http");
 
+const port = process.env.PORT || 3000;
+
 http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("online 🔥");
-}).listen(3000);
+}).listen(port);
 
 setInterval(() => {
   console.log("farmando... 🔥");
